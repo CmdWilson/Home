3166821d0dc474e4c4ff2051db2fc7df31d151fe
CmdWilson <141258229+CmdWilson@users.noreply.github.com>
2023-08-25 23:39:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int max = 1;
+while (max <= a)
+{
+if (max % 2 == 0)
+{ 
+    Console.Write(max);
+    Console.Write(",");
+    max++;
+}
+else
+{
+    max++;
+}
+}
