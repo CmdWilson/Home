f533074d3c0ddab465b58e95727431af56571960
CmdWilson <141258229+CmdWilson@users.noreply.github.com>
2023-08-25 23:33:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("a = ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("b = ");
+int b = int.Parse(Console.ReadLine());
+if(a > b)
+{
+    Console.Write("max = ");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(b);
+}
